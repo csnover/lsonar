635679c92c0e1b9a2c3d1ac455735474307b1b98
Colin Snover <github.com@zetafleet.com>
2025-09-03 19:01:58 -0500

    Fix gmatch not always iterating to the end

--- src/lua/gmatch.rs
+++ src/lua/gmatch.rs
@@ -19,19 +19,12 @@ use std::borrow::Cow;
 ///
 /// Captured string positions are 1-indexed if the `1-based` feature is enabled.
 pub fn gmatch<'a>(s: &'a [u8], pattern: &[u8], init: Option<isize>) -> Result<GMatchIterator<'a>> {
-    let is_empty_pattern = pattern.is_empty();
-
-    let pattern_ast = if is_empty_pattern {
-        <_>::default()
-    } else {
-        parse_pattern(pattern)?
-    };
+    let pattern_ast = parse_pattern(pattern)?;
 
     Ok(GMatchIterator {
         bytes: s,
         pattern_ast,
         current_pos: calculate_start_index(s.len(), init),
-        is_empty_pattern,
     })
 }
 
@@ -39,7 +32,6 @@ pub struct GMatchIterator<'a> {
     pub(super) bytes: &'a [u8],
     pub(super) pattern_ast: AstRoot,
     pub(super) current_pos: usize,
-    pub(super) is_empty_pattern: bool,
 }
 
 impl<'a> Iterator for GMatchIterator<'a> {
@@ -50,36 +42,24 @@ impl<'a> Iterator for GMatchIterator<'a> {
             return None;
         }
 
-        if self.is_empty_pattern {
-            let result = Some(vec![Cow::Borrowed(
-                &self.bytes[self.current_pos..self.current_pos],
-            )]);
-            self.current_pos += 1;
-            return result;
-        }
-
-        find_first_match(&self.pattern_ast, self.bytes, self.current_pos).and_then(
+        find_first_match(&self.pattern_ast, self.bytes, self.current_pos).map(
             |MatchRanges {
                  full_match,
                  captures,
              }| {
+                self.current_pos = full_match.end;
                 if full_match.is_empty() {
-                    self.current_pos = full_match.end + 1;
-                    if self.current_pos > self.bytes.len() {
-                        return None;
-                    }
-                } else {
-                    self.current_pos = full_match.end;
+                    self.current_pos += 1;
                 }
 
-                Some(if captures.is_empty() {
+                if captures.is_empty() {
                     vec![Cow::Borrowed(&self.bytes[full_match])]
                 } else {
                     captures
                         .into_iter()
                         .map(|range| range.into_bytes(self.bytes))
                         .collect()
-                })
+                }
             },
         )
     }
